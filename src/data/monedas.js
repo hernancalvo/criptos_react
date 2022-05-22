c2593389aaa16ef53b9d6d84a06698b5bbe23265
hernancalvo <calvohernan1984@gmail.com>
2022-05-22 19:47:40 -0300

    Proyecto en React. Criptomonedas. Consulta a una API. Styled Components

---
+++ src/data/monedas.js
@@ -0,0 +1,11 @@
+const monedas = [
+    {id: "USD", nombre: "Dólar de Estados Unidos"},
+    {id: "ARS", nombre: "Peso Argentino"},
+    {id: "MXN", nombre: "Peso Mexicano"},
+    {id: "EUR", nombre: "Euro"},
+    {id: "GBP", nombre: "Libra Esterlina"}
+]
+
+export {
+    monedas
+}
